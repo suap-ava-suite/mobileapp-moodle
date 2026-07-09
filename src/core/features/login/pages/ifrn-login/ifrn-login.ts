4dcceed3721ceb9e163c0eadc8ca9e99a3c43b77
Matheus Luiz Soares <matheus.luiz1@academico.ifrn.edu.br>
2026-07-09 16:01:46 -0300

    criando pagina de login (em andamento)

--- src/core/features/login/pages/ifrn-login/ifrn-login.ts
+++ src/core/features/login/pages/ifrn-login/ifrn-login.ts
@@ -1,21 +1,37 @@
-// (C) Copyright 2015 Moodle Pty Ltd.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'page-ifrn-login',
-  templateUrl: './ifrn-login.html',
-  styleUrls: ['./ifrn-login.scss'],
+    selector: 'page-ifrn-login',
+    templateUrl: './ifrn-login.html',
+    styleUrls: ['./ifrn-login.scss'],
 })
-export class IfrnLoginPage {}
+export class IfrnLoginPage {
+
+    username = '';
+
+    password = '';
+
+    login(): void {
+        console.log('Login');
+
+        // Chamar a API do IFRN.
+    }
+
+    clear(): void {
+        this.username = '';
+        this.password = '';
+    }
+
+    forgotPassword(event: Event): void {
+        event.preventDefault();
+
+        // Abrir página de recuperação.
+    }
+
+    help(event: Event): void {
+        event.preventDefault();
+
+        // Abrir página de ajuda.
+    }
+
+}
