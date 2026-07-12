ab9b89a56a4cd10f3ec7e9449fd2447947b3861a
13rafaela <sopebr1209@provedor.com>
2026-07-12 20:14:18 -0300

    corrigindo problema de import para login.ts (concluido) e estilizando o login (em andamento)

--- src/core/features/login/pages/ifrn-login/ifrn-login.ts
+++ src/core/features/login/pages/ifrn-login/ifrn-login.ts
@@ -1,37 +1,28 @@
 import { Component } from '@angular/core';
-
+import { CoreSharedModule } from '@/core/shared.module';
 @Component({
     selector: 'page-ifrn-login',
     templateUrl: './ifrn-login.html',
     styleUrls: ['./ifrn-login.scss'],
+    imports: [
+        CoreSharedModule,
+    ],
 })
 export class IfrnLoginPage {
-
     username = '';
-
     password = '';
-
     login(): void {
         console.log('Login');
-
-        // Chamar a API do IFRN.
     }
-
     clear(): void {
         this.username = '';
         this.password = '';
     }
-
     forgotPassword(event: Event): void {
         event.preventDefault();
-
-        // Abrir página de recuperação.
     }
-
     help(event: Event): void {
         event.preventDefault();
-
-        // Abrir página de ajuda.
     }
 
 }
